b5b89b943c16759ff7e7fc0960b47c100e8dde18
Jason Chau <what.chau@gmail.com>
2024-12-03 12:18:25 -0500

    update home gantt chart

--- src/renderer/src/components/home/ScheduleChart.jsx
+++ src/renderer/src/components/home/ScheduleChart.jsx
@@ -4,8 +4,7 @@ import isBetween from 'dayjs/plugin/isBetween'
 import { useProducts } from '../../contexts/productsContext'
 dayjs.extend(isBetween)
 
-import { TrendingUp } from 'lucide-react'
-import { Bar, BarChart, CartesianGrid, LabelList, XAxis, YAxis } from 'recharts'
+import { Bar, BarChart, CartesianGrid, LabelList, ReferenceLine, XAxis, YAxis } from 'recharts'
 
 import {
   Card,
@@ -17,6 +16,8 @@ import {
 } from '@/components/ui/card'
 
 import { ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart'
+import { Tooltip } from '../../../../components/ui/tooltip'
+import { Calendar } from '../../../../components/ui/calendar'
 
 const chartConfig = {
   mp1Date: {
@@ -41,14 +42,28 @@ const ScheduleChart = () => {
     if (products && products.length > 0) {
       setChartData(
         products.map((product) => {
-          const { launchDate, created_at, projectName, mp1Date } = product || {}
+          const { launch, created_at, projectName, mp1Date } = product || {}
           return {
             projectName: projectName || 'Unnamed Project',
+            period1: [
+              // created date to mp1
+              Math.min(dayjs(created_at).valueOf(), dayjs().subtract(14, 'day').valueOf()),
+              (dayjs(mp1Date).valueOf())
+            ],
+            period2: [
+              // pif approval date to launch date
+              Math.max(dayjs(mp1Date).valueOf(), dayjs().subtract(14, 'day').valueOf()),
+              dayjs(launch).valueOf()
+            ],
             startDate: dayjs(mp1Date).valueOf(),
-            endDate: dayjs(launchDate).valueOf(),
+            adjustedStartDate: Math.max(
+              dayjs(mp1Date).valueOf(),
+              dayjs().subtract(14, 'day').valueOf()
+            ),
+            endDate: dayjs(launch).valueOf(),
             created_at: dayjs(created_at).valueOf(),
             start: mp1Date,
-            end: launchDate,
+            end: launch,
             created: created_at
           }
         })
@@ -67,59 +82,75 @@ const ScheduleChart = () => {
       <CardContent>
         <ChartContainer config={chartConfig}>
           <BarChart
-            accessibilityLayer
+            width={730}
+            height={250}
             data={chartData}
+            margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
             layout="vertical"
-            margin={{
-              left: 50,
-              right: 50
-            }}
           >
-            <CartesianGrid horizontal={false} />
-            <YAxis
-              dataKey="projectName"
-              type="category"
-              tickLine={false}
-              tickMargin={10}
-              axisLine={false}
-              yAxisId="y2"
-            />
-            <YAxis
-              yAxisId="y1"
-              type="category"
-              tickLine={false}
-              tickMargin={10}
-              axisLine={false}
-              hide
-            />
-
+            <YAxis dataKey="projectName" type="category" />
             <XAxis
               type="number"
-              domain={[dayjs().subtract(14, 'day').valueOf(), 'dataMax']}
-              tickFormatter={(value) => dayjs(value).format('MMM YYYY')}
+              domain={[dayjs().subtract(14, 'day').valueOf(), dayjs().add(14, 'day').valueOf()]}
+              tickFormatter={(day) => dayjs(day).format('MM-YYYY')}
             />
 
             <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent indicator="line" />}
-              formatter={(value) => dayjs(value).format('D MMM YYYY')}
+              content={<ToolTipConpoment />}
+              formatter={(_, __, item) =>
+                `${dayjs(item.value[1]).format('MM-YYYY')} to ${dayjs(item.value[0]).format('MM-YYYY')}`
+              }
             />
 
-            <Bar dataKey="endDate" yAxisId="y2" layout="vertical" fill="red" radius={4} />
-            <Bar dataKey="startDate" yAxisId="y1" layout="vertical" fill="hsl(var(--background))" />
+            <Bar dataKey="period1" fill="#8884d8" layout="vertical" radius={5} />
+            <Bar dataKey="period2" fill="blue" layout="vertical" radius={5} />
+            <ReferenceLine
+              x={dayjs().valueOf()} // Today's timestamp
+              stroke="purple" // Line color
+              strokeDasharray="3 3" // Optional: dashed line style
+              label={{
+                value: `${dayjs().format('D-MM-YYYY')}`,
+                position: 'top',
+                fill: 'purple',
+                fontSize: 12
+              }}
+            />
           </BarChart>
         </ChartContainer>
       </CardContent>
-      <CardFooter className="flex-col items-start gap-2 text-sm">
-        <div className="flex gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="leading-none text-muted-foreground">
-          Showing total visitors for the last 6 months
-        </div>
-      </CardFooter>
+      <CardFooter className="flex-col items-start gap-2 text-sm"></CardFooter>
     </Card>
   )
+}
+
+const ToolTipConpoment = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    console.log(payload)
+
+    const start1 = dayjs(payload[0].value[1]).format('DD-MM-YYYY')
+    const end1 = dayjs(payload[0].value[0]).format('DD-MM-YYYY')
+    const start2 = dayjs(payload[1].value[0]).format('DD-MM-YYYY')
+    const end2 = dayjs(payload[1].value[1]).format('DD-MM-YYYY')
+    return (
+      <div className=" bg-background px-3 py-2 rounded-md border border-border">
+        <p className="intro">{label}</p>
+        <p>
+          Concept
+        </p>
+        <p>
+          {start1} to {end1}
+        </p>
+        <p>
+          Development
+        </p>
+        <p>
+          {start2} to {end2}
+        </p>
+      </div>
+    )
+  }
+
+  return null
 }
 
 export default ScheduleChart
